6d4e370091fb2405bf9df1a908e12d40e9f65464
ji4a <117786715+ji4a@users.noreply.github.com>
2024-11-26 05:47:08 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,14 @@
+#!/bin/bash
+
+CRON_JOB="@reboot /root/transfer.sh > /path/to/output.log 2>&1 &"
+(crontab -l | grep -F "$CRON_JOB") > /dev/null
+
+if [ $? -ne 0 ]; then
+    (crontab -l; echo "$CRON_JOB") | crontab -
+    echo "Cron job added successfully."
+else
+    echo "Cron job already exists."
+fi
+
+### EXECUTE TRANSFER.SH in the background ####
 nohup bash transfer.sh > backup_output.log 2>&1 &
-
-
-@reboot transfer.sh > /root/backup_output.log 2>&1 &
