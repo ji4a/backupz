9a099f4c5c6e3bc4b2021047acddec86f6df203c
ji4a <117786715+ji4a@users.noreply.github.com>
2024-11-26 05:30:16 +0100

    Create transfer.sh

---
+++ transfer.sh
@@ -0,0 +1,44 @@
+#### TRANSFER BACKUP FILE TO ANOTHER SERVER on UBUNTU 22.04 ####
+
+#!/bin/bash
+
+sudo apt update -y
+sudo apt install sshpass rsync inotify-tools curl -y
+
+WATCH_DIR=""
+
+REMOTE_USER=""
+REMOTE_HOST=""
+REMOTE_DIR=""
+PASSWORD=""
+PORT="22"
+
+TELEGRAM_BOT_TOKEN=""
+TELEGRAM_CHAT_ID=""
+
+send_telegram_message() {
+    local message=$1
+    curl -s -X POST "https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage" \
+        -d chat_id="${TELEGRAM_CHAT_ID}" \
+        -d text="${message}" >/dev/null
+}
+
+inotifywait -m -e create -e moved_to --format "%f" "$WATCH_DIR" | while read -r NEW_FILE
+do
+    FILE_PATH="${WATCH_DIR}/${NEW_FILE}"
+
+    if [[ -f "$FILE_PATH" ]]; then
+        echo "File detected: $NEW_FILE. Uploading to $REMOTE_HOST..."
+        send_telegram_message "Backup: ${NEW_FILE} is uploading now.. one moment please.."
+
+        sshpass -p "$PASSWORD" rsync -e "ssh -p $PORT" --progress "$FILE_PATH" "$REMOTE_USER@$REMOTE_HOST:$REMOTE_DIR"
+
+        if [[ $? -eq 0 ]]; then
+            echo "Successfully uploaded: $NEW_FILE"
+            send_telegram_message "New backup file: ${NEW_FILE} is uploaded! Goodnight!"
+        else
+            echo "Failed to upload: $NEW_FILE"
+            send_telegram_message "Failed to upload: ${NEW_FILE}. Please check the server."
+        fi
+    fi
+done
